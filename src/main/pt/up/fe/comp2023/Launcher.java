098b34fc8f2e543c6963c495212901e7340f3473
Martim <mitram790@gmail.com>
2023-03-14 14:32:31 +0000

    Number of syntax errors is being reported

--- src/main/pt/up/fe/comp2023/Launcher.java
+++ src/main/pt/up/fe/comp2023/Launcher.java
@@ -36,10 +36,10 @@ public class Launcher {
 
         // Parse stage
         JmmParserResult parserResult = parser.parse(code, config);
-        System.out.println(parserResult.getRootNode().toTree());
         // Check if there are parsing errors
         TestUtils.noErrors(parserResult.getReports());
 
+        System.out.println(parserResult.getRootNode().toTree());
         // ... add remaining stages
     }
 
