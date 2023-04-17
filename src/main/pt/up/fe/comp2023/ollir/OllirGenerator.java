7fbefbddaa70ffd3342b5b7a66b9dcdbd7ca97f2
matiasfg <matiasfreitasguimaraes@gmail.com>
2023-04-17 01:39:46 +0100

    OllirGenerator updates

--- src/main/pt/up/fe/comp2023/ollir/OllirGenerator.java
+++ src/main/pt/up/fe/comp2023/ollir/OllirGenerator.java
@@ -1,12 +1,13 @@
 package pt.up.fe.comp2023.ollir;
 
 import pt.up.fe.comp.jmm.analysis.JmmSemanticsResult;
+import pt.up.fe.comp.jmm.ast.JmmNode;
 import pt.up.fe.comp.jmm.ollir.JmmOptimization;
 import pt.up.fe.comp.jmm.ollir.OllirResult;
+import pt.up.fe.comp.jmm.parser.JmmParserResult;
 import pt.up.fe.comp.jmm.report.Report;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class OllirGenerator implements JmmOptimization {
 
@@ -16,14 +17,78 @@ public class OllirGenerator implements JmmOptimization {
     }
 
     @Override
-    public OllirResult toOllir(JmmSemanticsResult jmmSemanticsResult) {
-        List<Report> reports = new ArrayList<>();
+    public  OllirResult toOllir(JmmSemanticsResult semanticsResult) {
+        JmmNode node = semanticsResult.getRootNode();
 
-        return new OllirResult(jmmSemanticsResult, "a", reports);
+        // Convert the AST to a String containing the equivalent OLLIR code
+        StringBuilder ollirCode = new StringBuilder(); // Convert node ...
+
+        iterateOverCode(semanticsResult.getRootNode(), ollirCode);
+        // More reports from this stage
+        List<Report> reports = new ArrayList<>();
+        Map<String, String> config = new HashMap<>();
+        JmmParserResult result = new JmmParserResult(semanticsResult.getRootNode(), reports, config);
+        return new OllirResult(semanticsResult, ollirCode.toString(), reports);
     }
 
     @Override
     public OllirResult optimize(OllirResult ollirResult) {
         return JmmOptimization.super.optimize(ollirResult);
     }
+
+
+    boolean hasReturn;
+
+    public String iterateOverCode(JmmNode rootNode, StringBuilder ollirCode) {
+        if(rootNode.getChildren().size() == 0 ){
+            if (rootNode.getKind() == "MethodDeclaration"){
+                ollirCode = dealMethodDeclaration(rootNode, ollirCode);
+            }
+            if (rootNode.getKind() == "Operation"){
+                dealWithOperation(rootNode, ollirCode);
+            }
+        }
+        else{
+            if (rootNode.getKind() == "MethodDeclaration"){
+                ollirCode = dealMethodDeclaration(rootNode, ollirCode);
+
+            }
+            if (rootNode.getKind() == "Operation"){
+                dealWithOperation(rootNode, ollirCode);
+            }
+            for (JmmNode childrenNode: rootNode.getChildren()) {
+                ollirCode = new StringBuilder(iterateOverCode(childrenNode, ollirCode));
+
+            }
+
+            if (rootNode.getKind() == "MethodDeclaration"){
+                ollirCode = finishMethodDeclaration(rootNode, ollirCode);
+            }
+        }
+
+
+        return ollirCode.toString();
+
+
+    }
+
+    private StringBuilder dealWithOperation(JmmNode jmmNode, StringBuilder ollirCode) {
+        if (jmmNode.getKind().equals("OPERATION"))
+            ollirCode.append(".i32 ");
+        return ollirCode;
+    }
+
+    private StringBuilder finishMethodDeclaration(JmmNode rootNode, StringBuilder ollirCode) {
+        String returnString = hasReturn ? "" : "\tret.V;\n\t";
+        ollirCode.append("\t").append(returnString).append("}\n");
+        hasReturn = false;
+        return ollirCode;
+    }
+
+    private StringBuilder dealMethodDeclaration(JmmNode rootNode, StringBuilder ollirCode) {
+        ollirCode.append("\t.method public ").append(rootNode.get("name")).append("(");
+        return ollirCode;
+    }
+
+
 }
