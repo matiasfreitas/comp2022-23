c59f5c752fbbdcb396d09353a2f7efc1861336b6
Martim Videira <mitram790@gmail.com>
2023-04-24 16:16:33 +0100

    JmmBuiltins with builtin types

---
+++ src/main/pt/up/fe/comp2023/analysis/JmmBuiltins.java
@@ -0,0 +1,33 @@
+package pt.up.fe.comp2023.analysis;
+
+import pt.up.fe.comp.jmm.analysis.table.Type;
+import pt.up.fe.comp.jmm.ast.JmmNode;
+
+import java.util.Optional;
+
+public class JmmBuiltins {
+
+    public static Type JmmInt = new Type("int",false);
+    public static Type JmmChar = new Type("char",false);
+    public static Type JmmBoolean = new Type("boolean",false);
+
+    public static Type JmmString = new Type("String",false);
+
+    public static Optional<Type> fromJmmNode(JmmNode node){
+        String kind = node.getKind();
+        Type t = switch (kind) {
+            case "Int" -> JmmInt;
+            case "Char" -> JmmChar;
+            case "Boolean" -> JmmBoolean;
+            case "String" -> JmmString;
+            default -> null;
+        };
+        System.out.println(t.toString());
+        if ( t == null){
+            return Optional.empty();
+        }
+        else{
+            return Optional.of(t);
+        }
+    }
+}
