62902d078b16b771727e65f9e0c2274f13461df1
Martim Videira <mitram790@gmail.com>
2023-04-25 23:48:10 +0100

    Checking If super class is being imported or is an available type

--- src/main/pt/up/fe/comp2023/analysis/semantics/SemanticContextAnalyser.java
+++ src/main/pt/up/fe/comp2023/analysis/semantics/SemanticContextAnalyser.java
@@ -1,10 +1,12 @@
 package pt.up.fe.comp2023.analysis.semantics;
 
+import pt.up.fe.comp.jmm.analysis.table.Type;
 import pt.up.fe.comp.jmm.ast.JmmNode;
 import pt.up.fe.comp.jmm.report.Report;
 import pt.up.fe.comp2023.analysis.symboltable.JmmSymbolTable;
 
 import java.util.List;
+import java.util.Optional;
 
 public class SemanticContextAnalyser extends ContextAnalyser<Void> {
 
@@ -15,9 +17,22 @@ public class SemanticContextAnalyser extends ContextAnalyser<Void> {
     @Override
     protected void buildVisitor() {
         this.addVisit("ImportDeclaration",this::handleImports);
+        this.addVisit("ClassDeclaration",this::handleClassDeclaration);
         this.addVisit("VarTypeSpecification", this::handleVarDeclaration);
         this.addVisit("MethodDeclaration",this::handleMethodDeclaration);
         this.setDefaultVisit(this::visitAllChildren);
+    }
+
+    private Void handleClassDeclaration(JmmNode jmmNode, List<Report> reports) {
+        System.out.println(jmmNode.getAttributes());
+        Optional<String> hasSuper = jmmNode.getOptional("extendsName");
+        if(hasSuper.isPresent()){
+            Type superType = new Type(hasSuper.get(),false);
+            if(!this.validType(superType)){
+                reports.add(this.createReport(jmmNode,"This class extends undefined type "+ superType+ " maybe try importing it!"));
+            }
+        }
+        return this.visitAllChildren(jmmNode,reports);
     }
 
     private Void handleMethodDeclaration(JmmNode jmmNode, List<Report> reports) {
