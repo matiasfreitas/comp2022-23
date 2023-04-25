b979ae67848c82f68de4a6340328ef65f43a51f7
Martim Videira <mitram790@gmail.com>
2023-04-25 21:50:41 +0100

    Symbol table now generates their own semantic errors

--- src/main/pt/up/fe/comp2023/analysis/Analyser.java
+++ src/main/pt/up/fe/comp2023/analysis/Analyser.java
@@ -11,6 +11,7 @@ import pt.up.fe.comp2023.analysis.symboltable.JmmSymbolTable;
 import pt.up.fe.comp2023.analysis.generators.symboltable.JmmSymbolTableGen;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Analyser implements JmmAnalysis {
@@ -18,7 +19,8 @@ public class Analyser implements JmmAnalysis {
     @Override
     public JmmSemanticsResult semanticAnalysis(JmmParserResult jmmParserResult) {
         JmmSymbolTableGen symbolTableGen =  new JmmSymbolTableGen();
-        symbolTableGen.visit(jmmParserResult.getRootNode());
+        List<Report> symbolTableReports = new LinkedList<>();
+        symbolTableGen.visit(jmmParserResult.getRootNode(),symbolTableReports);
         JmmSymbolTable symbolTable = symbolTableGen.getJmmSymbolTable();
         System.out.println(symbolTable.print());
 
@@ -26,6 +28,7 @@ public class Analyser implements JmmAnalysis {
         System.out.println("Performing Analysis");
         List<Report> reports = semanticAnalyser.analyse();
         System.out.println("Endend Analysis");
+        reports.addAll(symbolTableReports);
         return new JmmSemanticsResult(jmmParserResult,symbolTable,reports);
     }
 }
