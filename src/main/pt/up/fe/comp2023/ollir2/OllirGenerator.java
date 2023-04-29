5ca8a9f07e805c53f7c2c1272ca2fdaedf489d61
Martim Videira <mitram790@gmail.com>
2023-04-29 18:18:31 +0100

    Optimization class that implements the JmmOptimization interface backbone

---
+++ src/main/pt/up/fe/comp2023/ollir2/OllirGenerator.java
@@ -0,0 +1,12 @@
+package pt.up.fe.comp2023.ollir2;
+
+import pt.up.fe.comp.jmm.ast.JmmNode;
+import pt.up.fe.comp.jmm.report.Report;
+
+import java.util.LinkedList;
+
+public class OllirGenerator {
+    public String generate(JmmNode rootNode, LinkedList<Report> ollirReports) {
+        return null;
+    }
+}
---
+++ src/main/pt/up/fe/comp2023/ollir2/Optimization.java
@@ -0,0 +1,22 @@
+package pt.up.fe.comp2023.ollir2;
+
+import pt.up.fe.comp.jmm.analysis.JmmSemanticsResult;
+import pt.up.fe.comp.jmm.ollir.JmmOptimization;
+import pt.up.fe.comp.jmm.ollir.OllirResult;
+import pt.up.fe.comp.jmm.report.Report;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Optimization implements JmmOptimization {
+    @Override
+    public OllirResult toOllir(JmmSemanticsResult jmmSemanticsResult) {
+        // TODO: Need to understand the pros and cons of var in java and auto in c++
+        var optimizedSemanticResult = optimize(jmmSemanticsResult);
+        var ollirGenerator = new OllirGenerator();
+        var rootNode =optimizedSemanticResult.getRootNode();
+        var ollirReports = new LinkedList<Report>();
+        var ollirCode  = ollirGenerator.generate(rootNode,ollirReports);
+        return new OllirResult(optimizedSemanticResult,ollirCode,ollirReports);
+    }
+}
