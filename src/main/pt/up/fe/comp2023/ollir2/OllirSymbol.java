fb9fb71d7c950ba70b1abf60961d7e9752fb3af9
Martim Videira <mitram790@gmail.com>
2023-04-30 16:24:51 +0100

    Conversion from Symbol to OllirSymbol

--- src/main/pt/up/fe/comp2023/ollir2/OllirSymbol.java
+++ src/main/pt/up/fe/comp2023/ollir2/OllirSymbol.java
@@ -1,9 +1,12 @@
 package pt.up.fe.comp2023.ollir2;
 
+import pt.up.fe.comp.jmm.analysis.table.Symbol;
 import pt.up.fe.comp.jmm.analysis.table.Type;
 import pt.up.fe.comp.jmm.ast.JmmNode;
+import pt.up.fe.comp2023.analysis.JmmBuiltins;
 
 import java.util.Optional;
+
 
 public record OllirSymbol(String value, String type) {
     public static OllirSymbol fromLiteral(JmmNode literal){
@@ -19,6 +22,36 @@ public record OllirSymbol(String value, String type) {
         System.out.println("New Ollir Symbol " + value + " " + type);
         return  new OllirSymbol(value,type);
     }
+    public static OllirSymbol fromSymbol(Symbol s){
+        String type = fromType(s.getType());
+        return  new OllirSymbol(s.getName(),type);
+    }
+
+    private static String fromType(Type type) {
+        String arrayPrefix= "";
+        String ollirType = "";
+        if(type.isArray()){
+            arrayPrefix= "array.";
+        }
+        Type compare = new Type(type.getName(),false);
+        if(compare.equals(JmmBuiltins.JmmInt)){
+            ollirType = "i32";
+        }
+        else if(compare.equals(JmmBuiltins.JmmBoolean)){
+            ollirType = "bool";
+        }
+        else if(compare.equals(JmmBuiltins.JmmString)){
+            ollirType = "String";
+        }
+        else if(compare.equals(JmmBuiltins.JmmVoid)){
+            ollirType = "V";
+        }
+        else{
+            ollirType = compare.getName();
+        }
+        return arrayPrefix + ollirType;
+    }
+
     public String toCode(){
         return value + "." + type;
     }
