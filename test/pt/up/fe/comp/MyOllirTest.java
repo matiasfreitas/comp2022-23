47139cf30632db560565497d2035c5fe7ffbd679
Martim Videira <mitram790@gmail.com>
2023-05-10 15:42:14 +0100

    Added testing ollir general function

--- test/pt/up/fe/comp/MyOllirTest.java
+++ test/pt/up/fe/comp/MyOllirTest.java
@@ -1,17 +1,12 @@
 package pt.up.fe.comp;
 
 import org.junit.Test;
-import org.specs.comp.ollir.ArrayType;
-import org.specs.comp.ollir.ClassType;
-import org.specs.comp.ollir.ElementType;
-import org.specs.comp.ollir.Type;
 import pt.up.fe.comp.jmm.ollir.OllirResult;
 import pt.up.fe.comp.jmm.report.Report;
 import pt.up.fe.comp.jmm.report.ReportType;
 import pt.up.fe.comp2023.analysis.symboltable.JmmSymbolTable;
 import pt.up.fe.comp2023.ollir2.OllirGenerator;
 import pt.up.fe.specs.util.SpecsIo;
-import pt.up.fe.specs.util.SpecsStrings;
 
 import java.util.HashMap;
 import java.util.LinkedList;
@@ -39,9 +34,8 @@ public class MyOllirTest {
     }
 
 
-    @Test
-    public void testHelloWorld(){
-        String jmmCode= SpecsIo.getResource("pt/up/fe/comp/myollir/helloWorld.jmm");
+    public void testOllir(String path){
+        String jmmCode= SpecsIo.getResource(path);
         var output = TestUtils.analyse(jmmCode);
         var rootNode = output.getRootNode();
         System.out.println(rootNode.toTree());
@@ -63,35 +57,18 @@ public class MyOllirTest {
         var reports = new LinkedList<Report>();
         var ollirCode = ollirGenerator.visit(rootNode,reports);
         System.out.println("Ollir Code:\n"+ ollirCode);
-
-    }
-
-    public String getType(Type type) {
-        if (type.getTypeOfElement() == ElementType.OBJECTREF) {
-            var classType = (ClassType) type;
-            return classType.getName();
-        }
-
-        if (type.getTypeOfElement() == ElementType.ARRAYREF) {
-            var arrayType = (ArrayType) type;
-            System.out.println("TYPE OF ELEMENT: " + arrayType.getTypeOfElement());
-            System.out.println("TYPE OF ELEMENTS: " + arrayType.getTypeOfElements());
-            return arrayType.getTypeOfElements().toString();
-        }
-
-        System.out.println("Not yet implemented: " + type.getTypeOfElement());
-        return type.toString();
     }
 
     @Test
-    public void testMyclass1() {
-        var result = getOllirResult("myclass1.ollir");
+    public void idTypeBug(){
+        var path = "pt/up/fe/comp/myollir/idTypeBug.jmm";
+        testOllir(path);
+    }
 
-        // result.getOllirClass().get
-
-        var methodName = getOllirResult("myclass1.ollir")
-                        .getOllirClass().getMethod(1).getMethodName();
-        assertEquals("sum", methodName);
+    @Test
+    public void helloWorld(){
+        var path = "pt/up/fe/comp/myollir/helloWorld.jmm";
+        testOllir(path);
     }
 
 
